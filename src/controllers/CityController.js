95bbb0513fb135208fbfda6981776d2f0c69574f
Frederico <fredrumond@gmail.com>
2021-04-21 11:58:13 -0300

    Deve retornar 400 se não foi informado o estado

--- src/controllers/CityController.js
+++ src/controllers/CityController.js
@@ -1,9 +1,13 @@
 module.exports = class CityController {
     async create(request,response){
-        const { nome } = request.body
+        const { nome, estado } = request.body
         try {
             if(!nome){
-                return response.status(400).json({message: '400' })
+                return response.status(400).json({message: 'O nome é obrigatório' })
+            }
+    
+            if(!estado){
+                return response.status(400).json({message: 'O estado é obrigatório' })
             }
         } catch (error) {
             return response.status(500).json({message: '500' })
--- tests/integration/city.test.js
+++ tests/integration/city.test.js
@@ -7,6 +7,17 @@ describe('Teste integracao cidade', () => {
           .post('/cidade')
           .send({})
         expect(res.status).toEqual(400)
+        expect(res.body.message).toEqual('O nome é obrigatório')
+      })
+
+      it('Deve retornar 400 se não foi informado o estado', async () => {
+        const res = await request(app)
+          .post('/cidade')
+          .send({
+              nome: "cidade_qualquer"
+          })
+        expect(res.status).toEqual(400)
+        expect(res.body.message).toEqual('O estado é obrigatório')
       })
   })
   
