9ff582b04c0bb9eb9935571da4be9d0c396c071c
Frederico <fredrumond@gmail.com>
2021-04-21 20:45:44 -0300

    Criando um MAIN_ROUTE para os casos de teste

--- tests/integration/city-show-state.test.js
+++ tests/integration/city-show-state.test.js
@@ -1,12 +1,13 @@
 const request = require('supertest')
 const app = require('../../src/main/app')
-
 const cityFaker = require('../utils/cityFaker')
+
+const MAIN_ROUTE = '/cidade';
 
 describe('Teste integracao buscar cidade pelo estado', () => {
     it('Deve retornar 500 se não foi informado um query param permitido', async () => {
         const res = await request(app)
-          .get('/cidade?busca=qualquer')
+          .get(`${MAIN_ROUTE}?busca=qualquer`)
 
         expect(res.status).toEqual(500)
         expect(res.body.message).toEqual('Ação não disponivel')
@@ -14,7 +15,7 @@ describe('Teste integracao buscar cidade pelo estado', () => {
 
     it('Deve retornar 404 se a cidade informada não foi encontrada', async () => {
       const res = await request(app)
-        .get('/cidade?estado=cidade')
+        .get(`${MAIN_ROUTE}?estado=cidade`)
 
       expect(res.status).toEqual(404)
       expect(res.body.message).toEqual('Estado não encontrado')
@@ -23,7 +24,7 @@ describe('Teste integracao buscar cidade pelo estado', () => {
     it('Deve retornar 200 se a cidade informada foi encontrada', async () => {
       const fakeCity = await cityFaker
       const res = await request(app)
-        .get(`/cidade?estado=${fakeCity.state}`)
+        .get(`${MAIN_ROUTE}?estado=${fakeCity.state}`)
 
       expect(res.status).toEqual(200)
       expect(res.body.message).toEqual('Estado encontrado')
--- tests/integration/client-show-name.test.js
+++ tests/integration/client-show-name.test.js
@@ -1,12 +1,13 @@
 const request = require('supertest')
 const app = require('../../src/main/app')
-
 const clientFaker = require('../utils/clientFaker')
+
+const MAIN_ROUTE = '/cliente';
 
 describe('Teste integracao buscar cliente pelo nome', () => {
     it('Deve retornar 500 se não foi informado um query param permitido', async () => {
         const res = await request(app)
-          .get('/cliente?busca=qualquer')
+          .get(`${MAIN_ROUTE}?busca=qualquer`)
 
         expect(res.status).toEqual(500)
         expect(res.body.message).toEqual('Ação não disponivel')
@@ -14,7 +15,7 @@ describe('Teste integracao buscar cliente pelo nome', () => {
 
     it('Deve retornar 404 se o cliente informado não foi encontrado', async () => {
       const res = await request(app)
-        .get(`/cliente?nome=aleatorio${Date.now()}`)
+        .get(`${MAIN_ROUTE}?nome=aleatorio${Date.now()}`)
 
       expect(res.status).toEqual(404)
       expect(res.body.message).toEqual('Cliente não encontrado')
@@ -23,7 +24,7 @@ describe('Teste integracao buscar cliente pelo nome', () => {
     it('Deve retornar 200 se o cliente informado foi encontrado', async () => {
       const fakeClient = await clientFaker
       const res = await request(app)
-        .get(`/cliente?nome=${fakeClient.name}`)
+        .get(`${MAIN_ROUTE}?nome=${fakeClient.name}`)
 
       expect(res.status).toEqual(200)
       expect(res.body.message).toEqual('Cliente encontrado')
@@ -32,7 +33,7 @@ describe('Teste integracao buscar cliente pelo nome', () => {
     it('Deve retornar 200 se o cliente informado pelo id foi encontrado', async () => {
       const fakeClient = await clientFaker
       const res = await request(app)
-        .get(`/cliente/${fakeClient.id}`)
+        .get(`${MAIN_ROUTE}/${fakeClient.id}`)
 
       expect(res.status).toEqual(200)
       expect(res.body.message).toEqual('Cliente encontrado')
