198d5efe9bceced5d5b1ebb8fbfbe9fd556f39d4
Frederico <fredrumond@gmail.com>
2021-04-22 20:50:01 -0300

    Removendo arquivos que nao estao sendo utilizados

--- jest-integration-config.js
+++
@@ -1,3 +0,0 @@
-const config = require('./jest.config')
-config.testMatch = ['**/tests/**/*.test.js']
-module.exports = config
--- jest-unit-config.js
+++
@@ -1,3 +0,0 @@
-const config = require('./jest.config')
-config.testMatch = ['**/tests/**/*.spec.js']
-module.exports = config
