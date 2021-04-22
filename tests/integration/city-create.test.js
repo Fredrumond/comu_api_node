aad6cd367355753bddbbc6c5752bfe0bbfb5ea1f
Frederico <fredrumond@gmail.com>
2021-04-21 22:21:31 -0300

    Criando Template para testes repetitivos de entradas invalidas

--- tests/integration/city-create.test.js
+++ tests/integration/city-create.test.js
@@ -3,34 +3,40 @@ const app = require('../../src/main/app')
 
 const MAIN_ROUTE = '/cidade';
 
+let city = {
+  name: `cidade${Date.now()}`,
+  state: `estado${Date.now()}`
+}
+
 describe('Teste integracao criar cidade', () => {
-    it('Deve retornar 400 se não foi informado o nome', async () => {
-        const res = await request(app)
-          .post(`${MAIN_ROUTE}`)
-          .send({})
-        expect(res.status).toEqual(400)
-        expect(res.body.message).toEqual('O nome é obrigatório')
-      })
-
-      it('Deve retornar 400 se não foi informado o estado', async () => {
-        const res = await request(app)
+    describe('Ao tentar inserir uma cidade invalida', () => {
+      const registerNewCityTemplate = async (newData, errorMessage) => {
+          const res = await request(app)
           .post(`${MAIN_ROUTE}`)
           .send({
-              name: "cidade_qualquer"
+            ...city, ...newData
           })
-        expect(res.status).toEqual(400)
-        expect(res.body.message).toEqual('O estado é obrigatório')
+          
+          expect(res.status).toEqual(400)
+          expect(res.body.message).toEqual(errorMessage)
+        }
+
+      test('Deve retornar 400 se não foi informado o nome', async () => {
+          await registerNewCityTemplate({ name: null }, 'O nome é obrigatório')
       })
 
-      it('Deve retornar 201 se a cidade foi cadastrada', async () => {
-        const res = await request(app)
-          .post(`${MAIN_ROUTE}`)
-          .send({
-              name: "cidade_qualquer",
-              state: "estado_qualquer"
-          })
-        expect(res.status).toEqual(201)
-        expect(res.body.message).toEqual('Cidade cadastrada com sucesso')
+      test('Deve retornar 400 se não foi informado o estado', async () => {
+          await registerNewCityTemplate({ state: null }, 'O estado é obrigatório')
       })
+
+    })
+
+    it('Deve retornar 201 se a cidade foi cadastrada', async () => {
+      const res = await request(app)
+        .post(`${MAIN_ROUTE}`)
+        .send(city)
+      expect(res.status).toEqual(201)
+      expect(res.body.message).toEqual('Cidade cadastrada com sucesso')
+    })
   })
   
---
+++ tests/integration/client-create.test.js
@@ -0,0 +1,69 @@
+const request = require('supertest')
+const app = require('../../src/main/app')
+
+const MAIN_ROUTE = '/cliente';
+
+let client = {
+  name: `client${Date.now()}`,
+  sex: 1,
+  birth_date: "08/02/1993",
+  age: 28,
+  id_city: 1
+}
+
+describe('Teste integracao cliente', () => {
+  describe('Ao tentar inserir um cliente invalido', () => {
+    const registerNewClientTemplate = async (newData, errorMessage) => {
+        const res = await request(app)
+        .post(`${MAIN_ROUTE}`)
+        .send({
+          ...client, ...newData
+        })
+        
+        expect(res.status).toEqual(400)
+        expect(res.body.message).toEqual(errorMessage)
+      }
+
+    test('Deve retornar 400 se não foi informado o nome', async () => {
+        await registerNewClientTemplate({ name: null }, 'O nome é obrigatório')
+    })
+
+    test('Deve retornar 400 se não foi informado o sexo', async () => {
+        await registerNewClientTemplate({ sex: null }, 'O sexo é obrigatório')
+    })
+
+    test('Deve retornar 400 se o sexo informado é inválido', async () => {
+        await registerNewClientTemplate({ sex: 'sex' }, 'O sexo informado não é valido')
+    })
+
+    test('Deve retornar 400 se não foi informado a data de nascimento', async () => {
+        await registerNewClientTemplate({ birth_date: null }, 'A data de nascimento é obrigatória')
+    })
+
+    test('Deve retornar 400 se não foi informado a idade', async () => {
+        await registerNewClientTemplate({ age: null }, 'A idade é obrigatória')
+    })
+
+    test('Deve retornar 400 se não foi informado a cidade', async () => {
+        await registerNewClientTemplate({ id_city: null }, 'A cidade é obrigatória')
+    })
+
+    test('Deve retornar 400 se a cidade informada não for um id', async () => {
+        await registerNewClientTemplate({ id_city: 'cidade_qualquer' }, 'A cidade informada não foi encontrada')
+    })
+
+    test('Deve retornar 400 se a cidade informada não foi encontrada', async () => {
+        await registerNewClientTemplate({ id_city: 99 }, 'A cidade informada não foi encontrada')
+    })
+
+  })
+
+  it('Deve retornar 201 se o cliente foi cadastrado', async () => {
+    const res = await request(app)
+      .post(`${MAIN_ROUTE}`)
+      .send(client)
+    expect(res.status).toEqual(201)
+    expect(res.body.message).toEqual('Cliente cadastrado com sucesso')
+  })
+})
+  
