dc202660781a9a62596a0fc71fac1590ebef5b6f
Frederico <fredrumond@gmail.com>
2021-04-23 12:05:03 -0300

    Adicionando cors

--- src/main/app.js
+++ src/main/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const setupRoutes = require('../main/routes')
 
 app.use(express.json())
+app.use(cors())
 
 setupRoutes(app)
 
